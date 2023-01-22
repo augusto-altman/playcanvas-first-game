1d3b1377792f40581ff2d3a1372f6bbb467c9c78
augusto-altman <augusto.altman@gmail.com>
2023-01-22 20:33:49 +1300

    Code cleanup, remove rigid body and add vertical movement

--- src/player-movement.ts
+++ src/player-movement.ts
@@ -1,84 +1,78 @@
 class PlayerMovement extends pc.ScriptType {
-  force: pc.Vec3;
-  eulers: pc.Vec3;
+  // State
+  _eulers: pc.Vec3;
 
+  // Attributes
   camera: pc.Entity; // TODO: can we get this from the hierarchy?
-  power: number;
   lookSpeed: number;
 
   _onMouseMove(e: any) {
-    this.eulers.x -= this.lookSpeed * e.dx;
-    this.eulers.y -= this.lookSpeed * e.dy;
+    const { _eulers, lookSpeed } = this;
+
+    _eulers.x -= lookSpeed * e.dx;
+    _eulers.y -= lookSpeed * e.dy;
   }
 
   initialize() {
-    this.force = new pc.Vec3();
-    this.eulers = new pc.Vec3();
+    this._eulers = new pc.Vec3();
 
-    const app = this.app;
+    this.app.mouse.on("mousemove", this._onMouseMove, this);
 
-    // Listen for mouse move events
-    app.mouse.on("mousemove", this._onMouseMove, this);
-
-    // Check for required components
     if (!this.entity.collision) {
       console.error(
-        "First Person Movement script needs to have a 'collision' component"
-      );
-    }
-
-    if (
-      !this.entity.rigidbody ||
-      this.entity.rigidbody.type !== pc.BODYTYPE_DYNAMIC
-    ) {
-      console.error(
-        "First Person Movement script needs to have a DYNAMIC 'rigidbody' component"
+        "Player movement script needs to have a 'collision' component"
       );
     }
   }
 
   update(dt: number): void {
-    const force = this.force;
-    const app = this.app;
+    const { app, camera, _eulers, entity } = this;
 
-    // Get camera directions to determine movement directions
-    const forward = this.camera.forward;
-    const right = this.camera.right;
+    const { forward, right } = camera; // Get camera directions to determine movement directions
 
-    // movement
-    let x = 0;
-    let z = 0;
+    let xChange = 0;
+    let yChange = 0;
+    let zChange = 0;
 
-    // Use W-A-S-D keys to move player
-    // Check for key presses
-    if (app.keyboard.isPressed(pc.KEY_A) || app.keyboard.isPressed(pc.KEY_Q)) {
-      x -= right.x;
-      z -= right.z;
+    if (app.keyboard.isPressed(pc.KEY_A)) {
+      xChange -= right.x;
+      zChange -= right.z;
     }
 
     if (app.keyboard.isPressed(pc.KEY_D)) {
-      x += right.x;
-      z += right.z;
+      xChange += right.x;
+      zChange += right.z;
     }
 
     if (app.keyboard.isPressed(pc.KEY_W)) {
-      x += forward.x;
-      z += forward.z;
+      xChange += forward.x;
+      zChange += forward.z;
     }
 
     if (app.keyboard.isPressed(pc.KEY_S)) {
-      x -= forward.x;
-      z -= forward.z;
+      xChange -= forward.x;
+      zChange -= forward.z;
     }
 
-    // use direction from keypresses to apply a force to the character
-    if (x !== 0 || z !== 0) {
-      (force.set(x, 0, z).normalize() as any).scale(this.power); // TODO: remove `as any` assertion
-      this.entity.rigidbody?.applyForce(force); // TODO: should not be undefined
+    if (app.keyboard.isPressed(pc.KEY_Q)) {
+      yChange += 1;
+    }
+
+    if (app.keyboard.isPressed(pc.KEY_E)) {
+      yChange -= 1;
+    }
+
+    if (xChange !== 0 || zChange !== 0 || yChange !== 0) {
+      const pos = entity.getPosition();
+      entity.setPosition(
+        pos.x + xChange / 10,
+        pos.y + yChange / 10,
+        pos.z + zChange / 10
+      );
     }
 
     // update camera angle from mouse events
-    this.camera.setLocalEulerAngles(this.eulers.y, this.eulers.x, 0);
+    camera.setLocalEulerAngles(_eulers.y, _eulers.x, 0);
   }
 }
 
@@ -87,12 +81,6 @@ pc.registerScript(PlayerMovement, "playerMovement");
 PlayerMovement.attributes.add("camera", {
   type: "entity",
   description: "Optional, assign a camera entity, otherwise one is created",
-});
-
-PlayerMovement.attributes.add("power", {
-  type: "number",
-  default: 2500,
-  description: "Adjusts the speed of player movement",
 });
 
 PlayerMovement.attributes.add("lookSpeed", {
