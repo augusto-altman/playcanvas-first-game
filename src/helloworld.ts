4f7195c4edd8d3435bd2be0b1c9cf3310fc6e6f0
augusto-altman <augusto.altman@gmail.com>
2023-01-22 19:45:24 +1300

    Added basic player movement

--- src/helloworld.ts
+++ src/helloworld.ts
@@ -1,12 +1,18 @@
 class HelloWorld extends pc.ScriptType {
-    text: string;
-    entityLink : pc.Entity;
+  text: string;
+  entityLink: pc.Entity;
 
-    initialize() {
-        console.log('Hello ' + this.text);
-    }
-};
+  initialize() {
+    console.log("Hello " + this.text);
+  }
 
-pc.registerScript(HelloWorld, 'helloWorld');
-HelloWorld.attributes.add('text', {type: 'string'});
-HelloWorld.attributes.add('entityLink', {type : 'entity'})
+  update(dt: number): void {
+    const mouse = this.app.mouse;
+
+    console.log("mouse.isPressed", mouse.isPressed(pc.MOUSEBUTTON_RIGHT));
+  }
+}
+
+pc.registerScript(HelloWorld, "helloWorld");
+HelloWorld.attributes.add("text", { type: "string" });
+HelloWorld.attributes.add("entityLink", { type: "entity" });
---
+++ src/player-movement.ts
@@ -0,0 +1,106 @@
+class PlayerMovement extends pc.ScriptType {
+  force: pc.Vec3;
+  eulers: pc.Vec3;
+
+  camera: pc.Entity; // TODO: can we get this from the hierarchy?
+  power: number;
+  lookSpeed: number;
+
+  _onMouseMove(e: any) {
+    // If pointer is disabled
+    // If the left mouse button is down update the camera from mouse movement
+    if (pc.Mouse.isPointerLocked() || e.buttons[0]) {
+      this.eulers.x -= this.lookSpeed * e.dx;
+      this.eulers.y -= this.lookSpeed * e.dy;
+    }
+  }
+
+  initialize() {
+    this.force = new pc.Vec3();
+    this.eulers = new pc.Vec3();
+
+    var app = this.app;
+
+    // Listen for mouse move events
+    app.mouse.on("mousemove", this._onMouseMove, this);
+
+    // Check for required components
+    if (!this.entity.collision) {
+      console.error(
+        "First Person Movement script needs to have a 'collision' component"
+      );
+    }
+
+    if (
+      !this.entity.rigidbody ||
+      this.entity.rigidbody.type !== pc.BODYTYPE_DYNAMIC
+    ) {
+      console.error(
+        "First Person Movement script needs to have a DYNAMIC 'rigidbody' component"
+      );
+    }
+  }
+
+  update(dt: number): void {
+    var force = this.force;
+    var app = this.app;
+
+    // Get camera directions to determine movement directions
+    var forward = this.camera.forward;
+    var right = this.camera.right;
+
+    // movement
+    var x = 0;
+    var z = 0;
+
+    // Use W-A-S-D keys to move player
+    // Check for key presses
+    if (app.keyboard.isPressed(pc.KEY_A) || app.keyboard.isPressed(pc.KEY_Q)) {
+      x -= right.x;
+      z -= right.z;
+    }
+
+    if (app.keyboard.isPressed(pc.KEY_D)) {
+      x += right.x;
+      z += right.z;
+    }
+
+    if (app.keyboard.isPressed(pc.KEY_W)) {
+      x += forward.x;
+      z += forward.z;
+    }
+
+    if (app.keyboard.isPressed(pc.KEY_S)) {
+      x -= forward.x;
+      z -= forward.z;
+    }
+
+    // use direction from keypresses to apply a force to the character
+    if (x !== 0 || z !== 0) {
+      (force.set(x, 0, z).normalize() as any).scale(this.power); // TODO: remove `as any` assertion
+      this.entity.rigidbody?.applyForce(force); // TODO: should not be undefined
+    }
+
+    // update camera angle from mouse events
+    this.camera.setLocalEulerAngles(this.eulers.y, this.eulers.x, 0);
+  }
+}
+
+pc.registerScript(PlayerMovement, "playerMovement");
+
+PlayerMovement.attributes.add("camera", {
+  type: "entity",
+  description: "Optional, assign a camera entity, otherwise one is created",
+});
+
+PlayerMovement.attributes.add("power", {
+  type: "number",
+  default: 2500,
+  description: "Adjusts the speed of player movement",
+});
+
+PlayerMovement.attributes.add("lookSpeed", {
+  type: "number",
+  default: 0.25,
+  description: "Adjusts the sensitivity of looking",
+});
